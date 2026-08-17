79a473673e9cda29a8cbb07d0423d7154e3840b7
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-17 14:26:02 +0300

    fix: correct password hashing configuration

--- config/hashing.php
+++ config/hashing.php
@@ -1,2 +1,2 @@
 <?php
-return ['driver'=>env('HASH_DRIVER','bcrypt'),'bcrypt'=>['rounds'=>(int)env('BCRYPT_ROUNDS',12,'')],'argon'=>['memory'=>65536,'threads'=>1,'time'=>4]];
+return ['driver'=>env('HASH_DRIVER','bcrypt'),'bcrypt'=>['rounds'=>(int)env('BCRYPT_ROUNDS',12)],'argon'=>['memory'=>65536,'threads'=>1,'time'=>4]];
