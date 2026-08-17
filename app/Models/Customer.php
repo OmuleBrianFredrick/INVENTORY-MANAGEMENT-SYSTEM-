8b52503a4025fe1edc0bc224679d8f39ccbf75b4
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-17 14:42:50 +0300

    feat: add Customer model

---
+++ app/Models/Customer.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;use Illuminate\Database\Eloquent\SoftDeletes;
+class Customer extends Model {use SoftDeletes;protected $fillable=['name','email','phone','address','status','notes'];public function orders(){return $this->hasMany(Order::class);}}
