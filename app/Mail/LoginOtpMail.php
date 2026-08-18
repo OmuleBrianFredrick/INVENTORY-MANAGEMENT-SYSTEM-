e64580ca2e64255c06d95434400065c1acd609f1
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-18 19:28:24 +0300

    Rebrand OTP email

--- app/Mail/LoginOtpMail.php
+++ app/Mail/LoginOtpMail.php
@@ -1,4 +1,4 @@
 <?php
 namespace App\Mail;
 use Illuminate\Bus\Queueable;use Illuminate\Mail\Mailable;use Illuminate\Queue\SerializesModels;
-class LoginOtpMail extends Mailable {use Queueable,SerializesModels;public function __construct(public string $code,public string $name,public int $expiresInMinutes=5){}public function build(){return $this->subject('Your Inventory System sign-in code')->view('emails.login-otp');}}
+class LoginOtpMail extends Mailable {use Queueable,SerializesModels;public function __construct(public string $code,public string $name,public int $expiresInMinutes=5){}public function build(){return $this->subject('UJUZI SHOP MALL sign-in verification code')->view('emails.login-otp');}}
